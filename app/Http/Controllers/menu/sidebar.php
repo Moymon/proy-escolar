7e6d97080cc5b2c2843dee7d7f097806a4f8b959
Moymon <47367207+Moymon@users.noreply.github.com>
2023-06-28 15:06:04 -0600

    reestructura
    Se cambio el orden de los archivos para mostrarse como la pagina y asi tener mas control

--- app/Http/Controllers/menu/sidebar.php
+++ app/Http/Controllers/menu/sidebar.php
@@ -7,58 +7,77 @@ use Illuminate\Http\Request;
 
 class sidebar extends Controller
 {
+    /**Licenciatura-------------------------------------------------------**/
+    
+        /*Alumnos*/
+        public function alumnos_licenciatura(){
+            return view('licenciatura.alumnos.alumnos-licenciatura');
+        }
 
-    /*Alumnos-------------------------------------------------------------------------------*/
-    public function alumnos_licenciatura(){
-        return view('alumnos.alumnos_licenciatura');
-    }
+        /*Kardex*/
+        public function kardex_lic(){
+            return view('licenciatura.kardex.kardex-impresion-licenciatura');
+        }
 
-    public function alumnos_posgrado(){
-        return view('alumnos.alumnos_posgrado');
-    }
+        public function verificacion_lic(){
+            return view('licenciatura.kardex.kardex-verificacion-licenciatura');
+        }
+
+        /*Examenes*/
+        public function examenes_regularizacion(){
+        return view('licenciatura.examenes.examenesregularizacion_lic');
+        }
+
+        public function examenes_titulo()
+        {
+            return view('licenciatura.examenes.examenestitulo_lic');
+        }
+
+        public function listado_examenes(){
+            return view('licenciatura.examenes.listadoexamenes_lic');
+        }
+
+        public function fechas_et_er(){
+            return view('licenciatura.examenes.fechaseter_lic');
+        }
+
+        public function ordenes_pago()
+        {
+            return view('licenciatura.examenes.ordenes_lic');
+        }
+
+        public function captura_ex_reg(){ //****** */
+            return view('licenciatura.examenes.captura_ex_reg_lic');
+        }
+
+        /*Rutas para Procedimientos-------------------------------------------------------------*/
+        public function procedimientos_archivos_constancias(){
+            return view('licenciatura.procedimientos.menuprincipal');
+        }
+    
+    /*Posgrado-------------------------------------------------------------*/
+    
+        /*Alumnos----------------------------------------------------------*/
+        public function alumnos_posgrado(){
+            return view('posgrado.alumnos.alumnos_posgrado');
+        }
+
+    /*Adminsitracion-------------------------------------------------------*/
+    
+
+    
 
     //
     /*Rutas para Kardex---------------------------------------------------------------------*/
-    public function kardex_lic(){
-        return view('kardex.kardex-index-lic');
-    }
+    
 
-    public function verificacion_lic(){
-        return view('kardex.kardex-verificacion-lic');
-    }
+    
 
     //
     /*------------------Rutas para Examenes ET y ET-------------------------------------------*/
-    public function examenes_regularizacion(){
-        return view('examenes_et_er.examenesregularizacion');
-    }
-
-    public function examenes_titulo()
-    {
-        return view('examenes_et_er.examenestitulo');
-    }
-
-    public function listado_examenes(){
-        return view('examenes_et_er.listadoexamenes');
-    }
-
-    public function fechas_et_er(){
-        return view('examenes_et_er.fechaseter');
-    }
-
-    public function ordenes_pago()
-    {
-        return view('examenes_et_er.ordenes');
-    }
-
-    public function captura_ex_reg(){ //****** */
-        return view('examenes_et_er.captura_ex_reg');
-    }
     
-    /*Rutas para Procedimientos-------------------------------------------------------------*/
-    public function procedimientos_archivos_constancias(){
-        return view('procedimientos.menuprincipal');
-    }
+    
+    
 
     /*Rutas para Administracion-------------------------------------------------------------*/
     /*Funcion Index que mostrara los usuarios*/
